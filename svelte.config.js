45cb358526a3c9be9d88ac381d7c311d838a151b
Dmitry Holodov <dimalinux@protonmail.com>
2023-10-01 04:43:21 +0700

    added github actions

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,7 @@
 import adapter from '@sveltejs/adapter-static';
 
+const dev = process.argv.includes('dev');
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
@@ -7,6 +9,9 @@ const config = {
 			pages: 'docs/',
 			assets: 'docs'
 		}),
+		paths: {
+			base: dev ? '' : process.env.BASE_PATH,
+		}
 	}
 };
 
